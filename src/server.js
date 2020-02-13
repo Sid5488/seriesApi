7911e2cc968dc541ebb1867a31e318547441719d
Sid5488 <guilhermenoobsaibot@gmail.com>
2020-02-13 16:52:21 -0300

    Access arrunado no server.JS

--- src/server.js
+++ src/server.js
@@ -5,7 +5,8 @@ app.use(express.json());
 
 let allowCrossDomain = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', '*')
     next();
 }
 
